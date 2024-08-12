2e15c063d6e2fc5f4177cb4b3c485463b1294f03
programmerAJEU <ajeugomez@gmail.com>
2024-08-12 09:52:27 -0600

    change button color

--- src/_estilos/botones.tsx
+++ src/_estilos/botones.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const botones = StyleSheet.create({
     btn: {
         flexDirection: 'row',
-        backgroundColor: 'blue',
+        backgroundColor: '#03346E',
         width: '95%',
         padding: 10,
         justifyContent: 'space-between'
